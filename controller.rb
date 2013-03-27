d06ea4fa6ff1b8dc5fe32460ed223d074b316893
Rasmus Ahlbäck <rasmus.ahlback@elev.it-gymnasiet.se>
2013-03-27 16:55:46 +0100

    added comments to ruby code, removed unnecessary code

--- controller.rb
+++ controller.rb
@@ -1,68 +1,61 @@
 require 'sinatra'
 
+# Code by Rasmus Ahlbäck
 
-class MainController
+class MainController # class for getting data from files
 	def initialize
 	end
 
-	def find_text(search_term)
+	def find_text(search_term) # find the text for the current page
 		Dir.glob("./public/text/*").each do |object|
-			if search_term == File.basename(object)
+			if search_term == File.basename(object) #match text file with url
 				return false
 			end
 		end
 		return true
 	end
 
-	def find_pictures(search_term)
+	def find_pictures(search_term) #find pictures for the current page
 		checker = []
 		Dir.glob("./public/bilder/#{search_term}/*").each do |picture|
-			checker << picture
+			checker << picture # add pictures from files into list
 		end
 		if checker.size >= 1
-			return checker
+			return checker # only return list if there are pictures in it
 		else
-			return ["false"]
+			return ["false"] # prevent error
 		end
 	end
 
 end
 
-@@MainController = MainController.new
+@@MainController = MainController.new 
 
 
 layout 'layout.erb'
 
 
-not_found do
-	@broken = true
-	erb :'index.html'
+not_found do # in case the page does not exists
+	redirect '/hittade%20inte%20sidan'
 end
 
 
-@@files = Dir.glob("*/**.html.erb")
-
-get '/test' do 
-	"HEJ #{@@files}"
-end
-
-
-get '/' do
+get '/' do # get starting page
 	@bilder = ["false"]
 	@paragraph = File.readlines("./public/text/start")
 	@param = "start"
-	@cool_text = "cool_text"
+	@cool_text = "cool_text" # centering text
 	@page = ""
 	erb :'page.html'
 end
 
-get '/:page' do
+get '/:page' do # get all content pages based on url
 	@cool_text = "not_cool_text"
 	if @@MainController.find_text(params[:page]) == false
 		@bilder = @@MainController.find_pictures(params[:page])
 		@paragraph = File.readlines("./public/text/#{params[:page]}")
 		@param = params[:page]
-	else
+	else # if no page exists, display "not found text"
 		@bilder = ["No pic"]
 		@paragraph = File.readlines("./public/text/notfound")
 		@param = false
@@ -71,7 +64,7 @@ get '/:page' do
 		erb :'page.html'
 end
 
-get '/bilder/:zon' do
+get '/bilder/:zon' do # get pictures 
 	@get_pics = Dir.glob("./public/bilder/#{params[:zon]}/*.jpg")
-	erb :'bilder.html', {:layout => :"bild_layout"}
+	erb :'bilder.html', {:layout => :"bild_layout"} # change layout to gallery layout
 end
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Webbutvecklings projekt
+
+Det här är ett projekt som vi fick i vår webbutvecklings kurs, uppgiften var att skapa en hemsida som är en förbättring av en annan sida.
+
+I mitt projekt valde jag med hjälp av min lärare webbsidan www.vivamos.nu
